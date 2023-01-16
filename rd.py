15fd5fb869662ddd5061e6cfb95cf98cc54c4aec
Matthew Farrugia-Roberts <matt@far.in.net>
2023-01-16 17:20:10 +1100

    misc

--- rd.py
+++ rd.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 import fnmatch
 
 PATHS = [
@@ -12,34 +13,64 @@ PATHS = [
 def main():
     # Interpret command line arguments
     if not (args := sys.argv[1:]):
-        print("usage: rd <query string>", file=sys.stderr)
+        print("usage: rd [-r] [-d] <query string>", file=sys.stderr)
         sys.exit(1)
+    REM = False
+    DIR = False
+    while args[0][0] == "-":
+        if args[0][1] == "r":
+            REM = True
+        if args[0][1] == "d":
+            DIR = True
+        args.pop(0)
     
     # Perform search
     # TODO: smarter queries? fuzzy finding, etc.?
     pattern = " ".join(args) + "*"
     matches = list(find_files(pattern, *PATHS))
     print("found", len(matches), "matches")
+    
+    # Sort results
+    matches = sorted(matches, key=os.path.basename)
 
     # Display results
     for index, match in enumerate(matches):
         file = os.path.basename(match)
-        print(f"{index:x} {file[:60]}")
-    if not matches:
-        print("sorry, bye!")
-        sys.exit(2)
+        print(f"{index:d} {file[:60]}")
 
     # Offer to open some results
-    to_open = input("open? ")
-    for index_str in to_open.split():
-        if index_str.endswith("d"):
-            # open containing directory
-            index = int(index_str[:-1], base=16)
+    if len(matches) == 0:
+        print("sorry, bye!")
+        sys.exit(2)
+    elif len(matches) == 1:
+        # TODO: ALLOW OPEN CONTAINING DIRECTORY
+        print("opening", matches[0])
+        to_open = ["0"]
+    else:
+        to_open = input("open? ").split()
+
+    # Open some results
+    for index_str in to_open:
+        dir = DIR
+        rem = REM
+        while not index_str[-1].isdigit():
+            if index_str.endswith("d"):
+                dir = True
+            if index_str.endswith("r"):
+                rem = True
+            index_str = index_str[:-1]
+        index = int(index_str)
+        if dir:
             open_file(os.path.dirname(matches[index]))
+        elif rem:
+            push_file(matches[index])
         else:
-            index = int(index_str, base=16)
             open_file(matches[index])
 
+def push_file(src):
+    # TODO: Push straight to rem
+    dst = os.path.join("/Users/matt/Desktop", os.path.basename(src))
+    shutil.copy(src, dst)
 
 def open_file(path):
     os.system(f'open "{path}"')
